ed86ad47fd8694073f455ace358525f148a9f601
leuterkil <djfreedomofficial@gmail.com>
2021-06-01 17:36:30 +0300

    Commit #8 Finish Home Page Back-End (Half)

--- app/src/main/java/com/left4dev/leledometrostratou/home/Home.java
+++ app/src/main/java/com/left4dev/leledometrostratou/home/Home.java
@@ -1,7 +1,9 @@
 package com.left4dev.leledometrostratou.home;
 
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,21 +14,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.navigation.NavigationView;
 import com.left4dev.leledometrostratou.R;
 import com.left4dev.leledometrostratou.functions.Datas;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Home extends Fragment {
 
     private HomeViewModel mViewModel;
-    private TextView headerTitle,headerSubtitle,headerESSO,headerSeries;
-    private ImageView headerImage;
+    private TextView headerTitle,headerSubtitle,headerESSO,headerSeries,KS,Ypiretithikan,Synolo,Percent;
+    private TextView NameOfSoldier,NameOfRank;
+    private ImageView headerImage,SoldierImage,RankImage;
     private Datas data = new Datas();
     private ArrayList<String> userData;
+    private FloatingActionMenu floatingActionMenu;
+    private FloatingActionButton services,vacation,outs,penalty;
+    private ProgressBar progressBar;
 
     public static Home newInstance() {
         return new Home();
@@ -40,11 +58,7 @@ public class Home extends Fragment {
         View header = navigationView.getHeaderView(0);
 
 //        initialize components
-        headerTitle = header.findViewById(R.id.headTitle);
-        headerSubtitle = header.findViewById(R.id.headSubTitle);
-        headerESSO = header.findViewById(R.id.textViewEsso);
-        headerSeries = header.findViewById(R.id.textViewSeries);
-        headerImage = header.findViewById(R.id.headerImage);
+        InitializeComponents(fragmentView,header);
         userData = data.loadXML(getActivity());
 
         headerTitle.setText(userData.get(0));
@@ -52,13 +66,105 @@ public class Home extends Fragment {
         headerSeries.setText("Σειρά: "+userData.get(4));
         headerSubtitle.setText(userData.get(6));
         headerImage.setImageResource(Integer.parseInt(userData.get(5)));
+        NameOfSoldier.setText(userData.get(0));
+
+        floatingActionMenu.setClosedOnTouchOutside(true);
         return fragmentView;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        String myFormat = "dd  MMM  yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        Date Start=new Date();
+        Date End=new Date();
+        Date c = Calendar.getInstance().getTime();
+        try {
+            Start = sdf.parse(userData.get(1));
+            End = sdf.parse(userData.get(2));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         mViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        SoldierImage.setImageResource(mViewModel.CheckCorp(userData.get(6)));
+        if (Integer.parseInt(mViewModel.KS(c,End))>Integer.parseInt(mViewModel.KS(Start,End)))
+        {
+            KS.setText((mViewModel.KS(Start, End)) + "ΚΣ");
+        }
+        else if (Integer.parseInt(mViewModel.KS(c,End))<0)
+        {
+           KS.setText("0ΚΣ");
+        }
+        else
+        {
+            KS.setText((mViewModel.KS(c, End)) + "ΚΣ");
+        }
+
+
+
+
+        if (Integer.parseInt(mViewModel.KS(Start,c))<0)
+        {
+            Ypiretithikan.setText("0");
+        } else if (Integer.parseInt(mViewModel.KS(Start, c)) > Integer.parseInt(mViewModel.KS(Start, End))) {
+
+            Ypiretithikan.setText(mViewModel.KS(Start,End));
+        } else {
+            Ypiretithikan.setText(mViewModel.KS(Start, c));
+        }
+
+
+
+        Synolo.setText(mViewModel.KS(Start,End));
+        float synolo,ypiretithikan;
+        synolo = Float.parseFloat(mViewModel.KS(Start,End));
+        ypiretithikan = Float.parseFloat(mViewModel.KS(Start,c));
+        float percent = (ypiretithikan/synolo)*100;
+        DecimalFormat df = new DecimalFormat("0.0");
+
+
+
+        if (percent<0.0)
+        {
+            Percent.setText("0.0%");
+            progressBar.setProgress(0);
+        }
+        else if (percent>100.00)
+        {
+            Percent.setText("100.0%");
+            progressBar.setProgress(100);
+        }
+        else {
+            Percent.setText(df.format(percent) + "%");
+            progressBar.setProgress((int) percent);
+        }
+
+        RankImage.setImageResource(mViewModel.CheckRank(percent));
+        NameOfRank.setText(mViewModel.CheckRankName(percent,userData.get(6)));
     }
 
+    private void InitializeComponents(View fragmentView,View header)
+    {
+        headerTitle = header.findViewById(R.id.headTitle);
+        KS = fragmentView.findViewById(R.id.textViewMeres);
+        Ypiretithikan = fragmentView.findViewById(R.id.textViewYpiretithikan);
+        Percent = fragmentView.findViewById(R.id.textViewPercentage);
+        Synolo = fragmentView.findViewById(R.id.textViewSynolo);
+        progressBar = fragmentView.findViewById(R.id.progressBar);
+        headerSubtitle = header.findViewById(R.id.headSubTitle);
+        headerESSO = header.findViewById(R.id.textViewEsso);
+        headerSeries = header.findViewById(R.id.textViewSeries);
+        headerImage = header.findViewById(R.id.headerImage);
+        floatingActionMenu = fragmentView.findViewById(R.id.floatMenu);
+        penalty = floatingActionMenu.findViewById(R.id.penalty);
+        outs = floatingActionMenu.findViewById(R.id.out);
+        vacation = floatingActionMenu.findViewById(R.id.vacation);
+        services = floatingActionMenu.findViewById(R.id.services);
+        SoldierImage = fragmentView.findViewById(R.id.imageViewTyposSkoufou);
+        NameOfSoldier = fragmentView.findViewById(R.id.textViewNameOfSoldier);
+        NameOfRank = fragmentView.findViewById(R.id.textViewRankNameHome);
+        RankImage = fragmentView.findViewById(R.id.imageViewRankHome);
+    }
 }
--- app/src/main/java/com/left4dev/leledometrostratou/home/HomeViewModel.java
+++ app/src/main/java/com/left4dev/leledometrostratou/home/HomeViewModel.java
@@ -1,7 +1,293 @@
 package com.left4dev.leledometrostratou.home;
 
+import com.left4dev.leledometrostratou.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    public int CheckCorp(String corp)
+    {
+        int id;
+        switch (corp)
+        {
+            case "Τεθωρακισμένα":
+                id= R.drawable.tethorakismena;
+                break;
+            case "Καταδρομείς":
+                id = R.drawable.eidikesdynameis;
+                break;
+            case "Προεδρική Φρουρά":
+                id = R.drawable.peziko;
+                break;
+            case "Πολεμική Αεροπορεία":
+                id = R.drawable.aeroporeia;
+                break;
+            case "71η Αερομεταφερόμενοι":
+                id = R.drawable.aerometaferomenos;
+                break;
+            default:
+                id = R.drawable.peziko2;
+                break;
+
+        }
+        return id;
+    }
+
+    //1 minute = 60 seconds
+//1 hour = 60 x 60 = 3600
+//1 day = 3600 x 24 = 86400
+    public String KS(Date startDate, Date endDate) {
+        //milliseconds
+        String finalString;
+        long different = endDate.getTime() - startDate.getTime();
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = different / daysInMilli;
+        different = different % daysInMilli;
+
+        finalString = Long.toString(elapsedDays);
+        return finalString;
+
+    }
+
+    public int CheckRank(float percent)
+    {
+        int imageID = R.drawable.psarakas;
+        if (percent>=0.0 && percent<5.3)
+        {
+            imageID = R.drawable.psarakas;
+        }
+        else if (percent>=5.3 && percent<10.6)
+        {
+            imageID = R.drawable.arrouri;
+        }
+        else if(percent>=10.6&&percent<15.9)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_ypodekaneas;
+        }
+        else if(percent>=15.9&&percent<21.2)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_dekaneas;
+        }
+        else if (percent>=21.2&&percent<26.5)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_loxias;
+        }
+        else if (percent>=26.5&&percent<31.8)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_epiloxias;
+        }
+        else if (percent>=31.8&&percent<37.1)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_arxiloxias;
+        }
+        else if (percent>=37.1&&percent<42.4)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_antipaspistis;
+        }
+        else if (percent>=42.4&&percent<47.7)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_dea;
+        }
+        else if (percent>=47.7&&percent<53)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_anthipoloxagos;
+        }
+        else if (percent>=53&&percent<58.3)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_ypoloxagos;
+        }
+        else if (percent>=58.3&&percent<63.6)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_loxagos;
+        }
+        else if (percent>=63.6&&percent<68.9)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_tagmatarxis;
+        }
+        else if (percent>=68.9&&percent<74.2)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_antisintagmatarxis;
+        }
+        else if (percent>=74.2&&percent<79.5)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_sintagmatarxis;
+        }
+        else if (percent>=79.5&&percent<84.8)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_taxiarxos;
+        }
+        else if (percent>=84.8&&percent<90.1)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_ypostratigos;
+        }
+        else if (percent>=90.1&&percent<95.4)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_antistratigos;
+        }
+        else if (percent>=95.4&&percent<100.0)
+        {
+            imageID = R.drawable.diakritiko_stratos_xiras_stratigos;
+        }
+        else if (percent>=100.0)
+        {
+            imageID = R.drawable.lelele;
+        }
+        else if (percent<0)
+        {
+            imageID = R.drawable.psarakas;
+        }
+
+        return imageID;
+    }
+
+    public String CheckRankName(float percent,String Corp)
+    {
+        String Name = "Ψάρακας";
+        if (percent>=0.0 && percent<5.3)
+        {
+            Name = "Ψάρακας";
+        }
+        else if (percent>=5.3 && percent<10.6)
+        {
+            Name="Αρούρι";
+        }
+        else if(percent>=10.6&&percent<15.9)
+        {
+            Name = "Υποδεκανέας";
+        }
+        else if(percent>=15.9&&percent<21.2)
+        {
+            Name = "Δεκανέας";
+        }
+        else if (percent>=21.2&&percent<26.5)
+        {
+            Name = "Λοχίας";
+        }
+        else if (percent>=26.5&&percent<31.8)
+        {
+            Name = "Επιλοχίας";
+        }
+        else if (percent>=31.8&&percent<37.1)
+        {
+            Name = "Αρχιλοχίας";
+        }
+        else if (percent>=37.1&&percent<42.4)
+        {
+            Name = "Ανθυπασπιστής";
+        }
+        else if (percent>=42.4&&percent<47.7)
+        {
+            Name = "Δ.Ε.Α";
+        }
+        else if (percent>=47.7&&percent<53)
+        {
+            if (Corp.equals("Τεθωρακισμένα"))
+            {
+                Name = "Ανθυπίλαρχος";
+            }
+            else if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Ανθυπίατρος";
+            }
+            else
+            {
+                Name = "Ανθυπολοχαγός";
+            }
+        }
+        else if (percent>=53&&percent<58.3)
+        {
+            if (Corp.equals("Τεθωρακισμένα"))
+            {
+                Name = "Υπίλαρχος";
+            }
+            else if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Υπίατρος";
+            }
+            else
+            Name = "Υπολοχαγός";
+        }
+        else if (percent>=58.3&&percent<63.6)
+        {
+            if (Corp.equals("Τεθωρακισμένα"))
+            {
+                Name = "Ίλαρχος";
+            }
+            else if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Ιατρός";
+            }
+            else
+            Name = "Λοχαγός";
+        }
+        else if (percent>=63.6&&percent<68.9)
+        {
+            if (Corp.equals("Τεθωρακισμένα"))
+            {
+                Name = "Επίλαρχος";
+            }
+            else if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Επίατρος";
+            }
+            else
+            Name = "Ταγματάρχης";
+        }
+        else if (percent>=68.9&&percent<74.2)
+        {
+             if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Αρχίατρος";
+            }
+             else
+            Name = "Αντισυνταγματάρχης";
+        }
+        else if (percent>=74.2&&percent<79.5)
+        {
+            if (Corp.equals("Υγειονομικό"))
+            {
+                Name = "Γενικός Αρχίατρος";
+            }
+            else
+            Name = "Συνταγματάρχης";
+        }
+        else if (percent>=79.5&&percent<84.8)
+        {
+            Name = "Ταξίαρχος";
+        }
+        else if (percent>=84.8&&percent<90.1)
+        {
+            Name = "Υποστράτηγος";
+        }
+        else if (percent>=90.1&&percent<95.4)
+        {
+            Name = "Αντιστράτηγος";
+        }
+        else if (percent>=95.4&&percent<100.0)
+        {
+            Name = "Στρατηγός";
+        }
+        else if (percent>=100.0)
+        {
+            Name = "Λέλαρχος";
+        }
+        else if (percent<0)
+        {
+            Name = "Ψάρακας";
+        }
+
+        return Name;
+    }
 }
